161589ffc1e75c3704e1d13d635ff712071d972c
lucaruediger <luca@jrkm.ch>
2022-05-23 08:07:07 +0000

    test

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Rüdiger Luca und Halblützel Joshua Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
